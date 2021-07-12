f6a335f9c4ae6e694f3b656e308d128e6ac7a14d
EmployeeNikolaev <u302062@cheaz.ru>
2021-07-12 10:32:31 +0300

    page Hello

---
+++ tester/hello.php
@@ -0,0 +1,135 @@
+<?php
+    $folderRoot = "";
+    $link = "";
+    include("inc/functions/func_folderRoot.php");
+    include($folderRoot . "inc/functions/func_SESSION.php");
+    
+    require $folderRoot . 'conn/db.php';
+    $file_name = basename(__FILE__);
+    $user_uid = $_SESSION['uid'];
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <?php include($folderRoot . "inc/z_head.php"); ?>
+    <title>Тестирование</title>
+</head>
+<body>
+
+<!--left panel-->
+<?php include($folderRoot . "inc/z_rightPanel.php"); ?>
+
+<!--index-->
+<main>
+    <div class="container section">
+        <h3 class="center-align">Главная страница</h3>
+
+        <div class="row">
+            <ul class="collection with-header z-depth-1">
+                <li class="collection-header"><h4>Тесты</h4></li>
+                <?php
+                    include($folderRoot . "inc/functions/func_connectToDB_Tables.php");
+
+                    $TestIsEmpty = true;
+                    $select_quests = $link->query("SELECT name,table_ID, private, del, is_blocked, creator FROM list WHERE is_start ='true'");
+                    while ($r = $select_quests->fetch()) {
+                        if ($r['del'] == 'false') {
+                            if ($r['is_blocked'] == 'false') {
+                                $TestIsEmpty = false;
+                                echo "<li class='collection-item'>";
+
+                                // icon privat or YouDB
+                                $privatIcon = $r['private'] == 'true' ? "folder_special" : "folder";
+
+                                echo "<div><i class='material-icons'> ";
+                                if ($r['creator'] == $user_uid) {
+                                    echo "folder_shared";
+                                } else {
+                                    echo $privatIcon;
+                                }
+                                echo "</i>";
+
+                                // goToTest btn
+                                echo $r['name'] . "<a class='tooltipped right' href='";
+                                if ($r['is_blocked'] != 'true') {
+                                    echo $folderRoot . "pass/passquestion.php?db=" . $r['table_ID'];
+                                } else {
+                                    echo "#!";
+                                }
+
+                                //title
+                                $titleBlocked = $r['is_blocked'] != 'true' ? "Пройти тест" : "Заблокирован";
+                                echo "' data-position='bottom' data-tooltip='" . $titleBlocked . "' class='secondary-content'>";
+
+                                // icon is_blocked
+                                echo "<i class='material-icons ";
+                                if ($r['is_blocked'] != 'true') {
+                                    echo "'>send";
+                                } else {
+                                    echo "red'>block";
+                                }
+
+                                echo "</i></a></div>";
+                                echo "</li>";
+                            }
+                        }
+                    }
+                    if ($TestIsEmpty == true) {
+                        echo "<li class='collection-item'>";
+                        echo "<div>Пользователи еще не создавали тесты</div>";
+                        echo "</li>";
+                    }
+                ?>
+            </ul>
+        </div>
+
+        <div class="row">
+            <?php
+                $newsIsEmpty = true;
+                $select_news = $link->query("SELECT * FROM news ORDER BY id DESC LIMIT 5");
+                while ($news = $select_news->fetch()) {
+                    switch ($news['mode']) {
+                        case 'info':
+                            $newsIcon = "info";
+                            $newsColor = "blue";
+                            break;
+                        case 'warning':
+                            $newsIcon = "build";
+                            $newsColor = "orange";
+                            break;
+                        case 'error':
+                            $newsIcon = "bug_report";
+                            $newsColor = "red";
+                            break;
+                        default:
+                            $newsIcon = "";
+                            $newsColor = "";
+                            break;
+                    }
+
+                    $newsIsEmpty = false;
+                    echo "<ul class='collection with-header z-depth-1'>
+                            <li class='collection-header " . $newsColor . "'><h5><i class='material-icons left'>" . $newsIcon . "</i>
+                            " . $news['header'] . "<span class='right white-text'>" . date("d.m.y H:i", strtotime($news['date'])) . "</span></h5></li>";
+
+                    echo "<li class='collection-item'>" . $news['text'] . "</li></ul>";
+                }
+                if ($newsIsEmpty == true) {
+                    echo "<li class='collection-item'>";
+                    echo "<div>Новостей нет</div>";
+                    echo "</li></ul>";
+                }
+            ?>
+        </div><!-- row-->
+    </div><!-- class="container"-->
+</main>
+
+<?php
+    include($folderRoot . "inc/z_footer.php");
+?>
+<div class="sidenav-overlay" style="display: none; opacity: 0;"></div>
+<div class="drag-target"></div>
+
+</body>
+</html>
---
+++ tester/inc/functions/func_hello.php
@@ -0,0 +1,41 @@
+<?php
+    if (empty($_SESSION['second_name'])) {
+        $_SESSION['second_name'] = "ошибка";
+    }
+    if (empty($_SESSION['first_name'])) {
+        $_SESSION['first_name'] = "ошибка";
+    }
+    if (empty($_SESSION['patronymic'])) {
+        $_SESSION['patronymic'] = "ошибка";
+    }
+    if (empty($_SESSION['login'])) {
+        $_SESSION['login'] = "ошибка";
+    }
+    if (empty($_SESSION['email'])) {
+        $_SESSION['email'] = "ошибка";
+    }
+    if (empty($_SESSION['usertype'])) {
+        $_SESSION['usertype'] = 4;
+    }
+    if (empty($_SESSION['theme'])) {
+        $_SESSION['theme'] = 0;
+    }
+    if (empty($_SESSION['uid'])) {
+        $_SESSION['uid'] = 0;
+    }
+
+    function CancelIsLoging($folderRoot){
+        if (!empty($_SESSION['logged_user'])) {
+            header('Location: ' . $folderRoot . 'index.php');
+            exit;
+        }
+    }
+    function CancelIsLogout($folderRoot){
+        if (empty($_SESSION['logged_user'])) {
+            header('Location: ' . $folderRoot . 'account/account_login.php');
+            exit;
+        }
+    }
+
+
+?>
